e8eb186f47851f12ed8a99f7f83961c2b6b84884
Eunovo <eunovo9@gmail.com>
2022-03-20 02:13:38 +0100

    Implement filter and update methods

--- src/db/Repository.js
+++ src/db/Repository.js
@@ -3,16 +3,20 @@ const { excludeFields } = require("../utils/objects");
 
 class KnexRepository {
     constructor(table) {
-        this.table = knex(table);
+        this.table = table;
     }
 
     create(data) {
-        return this.table.insert(
+        return knex(this.table).insert(
             excludeFields(data, ['created_at', 'updated_at']));
     }
 
-    update(data, filter) {
+    find(filter = {}) {
+        return knex(this.table).where(filter).select();
+    }
 
+    update(data, filter) {
+        return knex(this.table).where(filter).update(data);
     }
 }
 
