7da97c05a70009fcb700210c7cf9bbadee108c22
Eunovo <eunovo9@gmail.com>
2022-03-20 02:14:11 +0100

    Implement API endpoints and test them

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
+const { knex } = require("./db/knex");
 const { KnexRepository } = require("./db/Repository");
-const { createToken, extractDataFromToken } = require("./utils/tokens");
+const { requireAuth } = require("./middleware/auth");
+const { createToken } = require("./utils/tokens");
 
 const users = new KnexRepository('users');
 const accounts = new KnexRepository('accounts');
@@ -10,28 +12,102 @@ const router = Router();
 
 router.post('/users', async (req, res) => {
     try {
-        const [id] = await users.create(req.body);
-        const token = createToken(id);
+        await knex.transaction(async trx => {
+            const [id] = await users.create(req.body).transacting(trx);
+            await accounts.create({ user_id: id }).transacting(trx);
+            const token = createToken(id);
 
-        res.json({
-            message: 'success',
-            token
+            res.json({
+                message: 'success',
+                token
+            });
         });
     } catch (error) {
         res.status(400).json({ message: 'Bad Input' });
     }
 });
 
-router.post('/accounts/fund', (req, res) => {
-
+router.get('/accounts', requireAuth, async (req, res) => {
+    const results = await accounts.find({ user_id: req.user.user_id });
+    res.json({
+        message: 'success',
+        accounts: results
+    });
 });
 
-router.post('/accounts/transfer', (req, res) => {
+router.post('/accounts/fund', requireAuth, async (req, res) => {
+    try {
+        const { account_id, amount } = req.body;
+        await knex.transaction(async trx => {
+            const [account] = await accounts.find({ account_id, user_id: req.user.user_id })
+                .transacting(trx);
+            if (!account) throw new Error("Account not found");
 
+            await accounts.update(
+                { account_id, balance: account.balance + amount },
+                { account_id }).transacting(trx);
+
+            res.json({ message: 'success' });
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
 
-router.post('/accounts/withdraw', (req, res) => {
+router.post('/accounts/transfer', requireAuth, async (req, res) => {
+    try {
+        const { sender_id, receiver_id, amount } = req.body;
+        await knex.transaction(async trx => {
+            const [sender] = await accounts.find({ account_id: sender_id, user_id: req.user.user_id })
+                .transacting(trx);
+            const [receiver] = await accounts.find({ account_id: receiver_id })
+                .transacting(trx);
 
+            if (!sender) throw new Error("Sender not found");
+            if (sender.balance < amount) throw new Error("Insufficient balance");
+            if (!receiver) throw new Error("Receiver not found");
+
+            await accounts.update(
+                { account_id: sender_id, balance: sender.balance - amount },
+                { account_id: sender_id }).transacting(trx);
+
+            await accounts.update(
+                { account_id: receiver_id, balance: receiver.balance + amount },
+                { account_id: receiver_id }).transacting(trx);
+
+            res.json({ message: 'success' });
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
 });
+
+router.post('/accounts/withdraw', requireAuth, async (req, res) => {
+    try {
+        const { account_id, amount } = req.body;
+        await knex.transaction(async trx => {
+            const [account] = await accounts.find({ account_id, user_id: req.user.user_id })
+                .transacting(trx);
+            if (!account) throw new Error("Account not found");
+            if (account.balance < amount) throw new Error("Insufficient balance");
+
+            await accounts.update(
+                { account_id, balance: account.balance - amount },
+                { account_id }).transacting(trx);
+
+            res.json({ message: 'success' });
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+router.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500)
+        .json({
+            message: 'Error'
+        });
+})
 
 module.exports = { router };
--- tests/api.test.js
+++ tests/api.test.js
@@ -6,7 +6,7 @@ beforeAll(async () => {
     await knex.migrate.latest();
 });
 
-afterAll(async () => {
+afterEach(async () => {
     await Promise.all(
         ['users', 'accounts'].map((table) => knex(table).where(true).del())
     );
@@ -40,21 +40,221 @@ it('should create the user account when all required fields are provided', async
     expect(response.body.token).toBeDefined();
 });
 
-it.skip('should allow the user perform fund and withdraw operation on the account', function (done) {
-    request(app)
-        .post('/user')
-        .expect(200, done);
+describe("Account funding tests", () => {
+    it('should allow the user fund all owned accounts', async function () {
+        let response = await makeUser('Novo');
+        const token = response.body.token;
+
+        response = await request(app)
+            .get('/accounts')
+            .set('Authorization', [`bearer ${token}`])
+            .expect(200);
+        const accounts = response.body.accounts;
+        const amount = 500;
+
+        for (const i in accounts) {
+            await makePostReq(
+                '/accounts/fund',
+                { account_id: accounts[i].account_id, amount },
+                token
+            ).expect(200);
+        }
+
+        response = await request(app)
+            .get('/accounts')
+            .set('Authorization', [`bearer ${token}`])
+            .expect(200);
+
+        for (const i in accounts) {
+            const previousBal = accounts[i].balance;
+            const currentBal = response.body.accounts[i].balance;
+            expect(currentBal).toEqual(previousBal + amount);
+        }
+    });
+
+    it('should not allow users to fund accounts not owned by them', async function () {
+        let response = await makeUser('Novo');
+        const novoToken = response.body.token;
+        response = await makeUser('Bob');
+        const bobToken = response.body.token;
+
+        await request(app)
+            .get('/accounts')
+            .expect(403);
+
+        response = await request(app)
+            .get('/accounts')
+            .set('Authorization', [`bearer ${novoToken}`])
+            .expect(200);
+
+        // Novo's accounts
+        const accounts = response.body.accounts;
+        for (const i in accounts) {
+            await makePostReq(
+                '/accounts/fund',
+                { account_id: accounts[i].account_id, amount: 100 },
+                bobToken
+            ).expect(400);
+        }
+    });
 });
 
-it.skip('should only allow the authenticated account owner to fund the account', function (done) {
-    request(app)
-        .post('/user')
-        .expect(200, done);
+describe("Account withdrawal tests", () => {
+    it('should allow the user withdraw from all owned accounts', async function () {
+        let response = await makeUser('Novo');
+        const token = response.body.token;
+
+        response = await request(app)
+            .get('/accounts')
+            .set('Authorization', [`bearer ${token}`])
+            .expect(200);
+        const accounts = response.body.accounts;
+        const amount = 500;
+
+        for (const i in accounts) {
+            await makePostReq(
+                '/accounts/fund',
+                { account_id: accounts[i].account_id, amount },
+                token
+            );
+
+            await makePostReq(
+                '/accounts/withdraw',
+                { account_id: accounts[i].account_id, amount },
+                token
+            ).expect(200);
+        }
+
+        response = await request(app)
+            .get('/accounts')
+            .set('Authorization', [`bearer ${token}`])
+            .expect(200);
+
+        for (const i in accounts) {
+            const currentBal = response.body.accounts[i].balance;
+            expect(currentBal).toEqual(0);
+        }
+    });
+
+    it('should not allow users to withdraw from accounts not owned by them', async function () {
+        let response = await makeUser('Novo');
+        const novoToken = response.body.token;
+        response = await makeUser('Bob');
+        const bobToken = response.body.token;
+
+        await request(app)
+            .get('/accounts')
+            .expect(403);
+
+        response = await request(app)
+            .get('/accounts')
+            .set('Authorization', [`bearer ${novoToken}`])
+            .expect(200);
+
+        // Novo's accounts
+        const accounts = response.body.accounts;
+        for (const i in accounts) {
+            await makePostReq(
+                '/accounts/withdraw',
+                { account_id: accounts[i].account_id, amount: 0 },
+                bobToken
+            ).expect(400);
+        }
+    });
+});
+
+describe("Account transfer tests", () => {
+    it('should allow inter-account transfer between accounts', async function () {
+        let response = await makeUser('Novo');
+        const novoToken = response.body.token;
+        response = await makeUser('Bob');
+        const bobToken = response.body.token;
+
+        const getAccounts = async (tokens) => (
+            (await Promise.all(
+                tokens.map((token) => (
+                    request(app)
+                        .get('/accounts')
+                        .set('Authorization', [`bearer ${token}`])
+                ))
+            )).map(res => res.body.accounts)
+        );
+
+        let [novoAccounts, bobAccounts] = await getAccounts([novoToken, bobToken]);
+
+        const amount = 500;
+        for (const i in novoAccounts) {
+            const account = novoAccounts[i];
+
+            await makePostReq(
+                '/accounts/fund',
+                { account_id: account.account_id, amount },
+                novoToken
+            );
+
+            await makePostReq(
+                '/accounts/transfer',
+                {
+                    sender_id: account.account_id,
+                    receiver_id: bobAccounts[i].account_id,
+                    amount
+                },
+                novoToken
+            ).expect(200);
+        }
+
+        [novoAccounts, bobAccounts] = await getAccounts([novoToken, bobToken]);
+
+        for (const i in novoAccounts) {
+            const senderBal = novoAccounts[i].balance;
+            const receiverBal = bobAccounts[i].balance;
+            expect(senderBal).toEqual(0);
+            expect(receiverBal).toEqual(amount);
+        }
+    });
+
+    it('should not allow users transfer from accounts not owned by them', async function () {
+        let response = await makeUser('Novo');
+        const novoToken = response.body.token;
+        response = await makeUser('Bob');
+        const bobToken = response.body.token;
+
+        let [novoAccounts, bobAccounts] = (await Promise.all(
+            [novoToken, bobToken].map((token) => (
+                request(app)
+                    .get('/accounts')
+                    .set('Authorization', [`bearer ${token}`])
+            ))
+        )).map(res => res.body.accounts);
+
+        const amount = 0;
+        for (const i in novoAccounts) {
+            // Bob tries to transfer from Novo's account to his own
+            // This transfer should not be successful
+            await makePostReq(
+                '/accounts/transfer',
+                {
+                    sender_id: novoAccounts[i].account_id,
+                    receiver_id: bobAccounts[i].account_id,
+                    amount
+                },
+                bobToken
+            ).expect(400);
+        }
+    });
 });
 
 
-it.skip('should allow inter-account transfer between existing accounts', function (done) {
-    request(app)
-        .post('/user')
-        .expect(200, done);
-});
+const makeUser = (name) => request(app).post('/users')
+    .send({ email: `${name}@gmail.com`, firstName: name, lastName: name });
+
+const makePostReq = (route, data, token) => {
+    const req = request(app)
+        .post(route);
+
+    if (token) {
+        req.set('Authorization', [`bearer ${token}`]);
+    }
+
+    return req.send(data);
+}
